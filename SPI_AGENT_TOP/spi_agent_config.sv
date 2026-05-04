10e83891e7088b3b9d223df7875a21caedf42598
Rahul Shukla <114052202+Rahul63076@users.noreply.github.com>
2026-05-04 10:01:41 +0530

    Add spi_agent_config class for SPI agent configuration

---
+++ SPI_AGENT_TOP/spi_agent_config.sv
@@ -0,0 +1,13 @@
+class spi_agent_config extends uvm_object;
+	`uvm_object_utils(spi_agent_config)
+	
+		
+	virtual spi_if vif;
+
+	uvm_active_passive_enum is_active = UVM_ACTIVE;
+
+	function new (string name = "spi_agent_config");
+		super.new(name);
+	endfunction
+
+endclass
